fed663c355f13bad2106bfcb8623f75b54e02523
ZHENGJIAN LIU <comezheng@gmail.com>
2020-07-26 19:45:53 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,16 +1,1 @@
-run_code_inside = false
-puts "Code before if...end"
-if run_code_inside
-  puts "Code inside"
-end
-puts "Code after if...end"
-
-
-chance_of_rain = 1
-puts "Let's go outside!"
-if chance_of_rain > 0.5
-  puts "Pack an umbrella!"
-else
-  puts "Enjoy the fine day!"
-end
-puts "Oh, and always wear sunscreen!"
+puts currentTime = Time.now.select_second
